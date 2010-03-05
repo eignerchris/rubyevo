766beec3e6549b296c83fab2eb66399c3b3be986
Chris Eigner <eignerchris@gmail.com>
2010-03-04 17:17:13 -0800

    adding main files

---
+++ evo_helpers.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/ruby
+
+module EvoHelpers 
+	# generates random chromosome of 0's and 1's. not very creative, will on average be 50/50 ratio
+	def random_chromosome
+		Array.new(32).collect {|e| e = rand 2}
+	end
+
+	# fitness function
+	# returns frequency of 1's in the array -- we'll use this as a basic scoring mechanism
+	def calc_fitness(member)
+		member.collect {|e| e if e == 1}.compact.size
+	end
+
+	# ZOMGSEX!1
+	# concatonates first half of chromo1 to second half of chromo2 with single mutation
+	def mate(chromo1, chromo2)
+		mutate(chromo1.first(16) + chromo2.last(16))
+	end
+
+	# mutates two values in chromosome
+	def mutate(chromo)
+		2.times { chromo[rand 32] = rand 2}
+		chromo
+	end
+
+	# returns top 10 fittest members
+	def alphas(arrs)
+		tmp = []
+		arrs.each_index {|i| tmp << [calc_fitness(arrs[i]),i]}
+		tmp.sort!.reverse!.collect {|e| arrs[e[1]]}.first 10
+	end
+
+	def print_alpha_stats(arr)
+		arr.each {|a| puts "#{a.inspect}, fitness: #{calc_fitness(a)}"}
+		puts ""
+	end
+end
+
+# open up array class to add several convenience methods!
+class Array
+	def random
+		self[rand self.size]
+	end
+
+	def is_spartan?
+		self == (Array.new(32).fill 1)
+	end
+end
---
+++ evolve.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/ruby
+# exercise in genetic programming
+# goal is to mate pairs of arrays until one reaches all 1's
+# that is...
+# 	1. start with 50, 32-member arrays with random 0's and 1's
+# 	2. calculate fitness of each (scored by number of 1's)
+# 	3. cross top 10 to form new arrays
+# 	4. goto 2
+
+require 'evo_helpers'
+include EvoHelpers
+
+start = Time.now
+NUM_OFFSPRING = ARGV[0].to_i
+SPARTAN = Array.new(32).fill 1
+SEEDS = Array.new(NUM_OFFSPRING).collect {random_chromosome}
+top10 = alphas(SEEDS)
+
+i = 0
+loop do
+	case top10.include? SPARTAN
+	when true
+		winners = top10.collect {|a| a if a.is_spartan?}.compact
+		puts "#{winners.size} member(s) reached spartan status!"
+		puts "completed in #{Time.now - start} seconds"
+		break
+	else
+		puts "*** ALPHAS FROM GENERATION: #{i} ***"
+		offspring = []
+		NUM_OFFSPRING.times do |j|
+			offspring << mate(top10.random, top10.random)
+		end 
+		top10 = alphas(offspring)
+		print_alpha_stats(top10)
+		i += 1
+	end
+end
